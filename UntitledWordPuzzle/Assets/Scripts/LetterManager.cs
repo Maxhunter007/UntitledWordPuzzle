c96db4dd1c628ba17e5dc6a5d18c77189b38c1e2
Phil <philine.bargen@stud-mail.uni-wuerzburg.de>
2023-11-05 12:36:21 +0100

    boxes spawn correctly
    and get destroyed!

--- UntitledWordPuzzle/Assets/Scripts/LetterManager.cs
+++ UntitledWordPuzzle/Assets/Scripts/LetterManager.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LetterManager : MonoBehaviour
@@ -5,12 +7,12 @@ public class LetterManager : MonoBehaviour
     [SerializeField] private GameObject ContainerPrefab;
 
     [SerializeField] private float ContainerDistance;
-    
+
     private Puzzle _activePuzzle;
     private GameObject[] _containers;
     private Sprite _aSprite;
     private Sprite _boxSprite;
-    
+
     public void Awake()
     {
         _aSprite = Resources.Load<Sprite>("Pics/A");
@@ -24,12 +26,12 @@ public class LetterManager : MonoBehaviour
         _boxSprite = Resources.Load<Sprite>("Pics/Box");
         _containers = new GameObject[_activePuzzle.LetterAmount];
 
-        var initializationPosition = -(ContainerDistance + _boxSprite.rect.width * ContainerPrefab.transform.localScale.x / _boxSprite.pixelsPerUnit / 2) * (_activePuzzle.LetterAmount-1);
-        
+        var initializationPosition = -(ContainerDistance + _boxSprite.rect.width * ContainerPrefab.transform.localScale.x / _boxSprite.pixelsPerUnit / 2) * (_activePuzzle.LetterAmount - 1);
+
         for (int i = 0; i < _activePuzzle.LetterAmount; i++)
         {
-            var gameObject = Instantiate(ContainerPrefab,this.gameObject.transform.parent);
-            gameObject.transform.localPosition = new Vector3(initializationPosition,7f);
+            var gameObject = Instantiate(ContainerPrefab, transform.parent);
+            gameObject.transform.localPosition = new Vector3(initializationPosition, 7f);
             initializationPosition += 2 * ContainerPrefab.transform.localScale.x * (ContainerDistance + _boxSprite.rect.width / _boxSprite.pixelsPerUnit / 2);
             _containers[i] = gameObject;
         }
@@ -37,10 +39,13 @@ public class LetterManager : MonoBehaviour
 
     public void Disable()
     {
-        //alle children durchgehen & löschen
-        for(int i = 0; i < transform.childCount; i++)
+        if (_activePuzzle)
         {
-            Destroy(transform.GetChild(0).gameObject);
+            for (int i = 0; i < _containers.Count(); i++)
+            {
+                Destroy(_containers[i]);
+            }
+            _containers = null;
         }
     }
 
