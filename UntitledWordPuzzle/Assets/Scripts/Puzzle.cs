a58e3e602e0ba9c87d1f244b535d19c926aed293
Phil <philine.bargen@stud-mail.uni-wuerzburg.de>
2023-11-05 13:39:09 +0100

    Update puzzle to get solved

--- UntitledWordPuzzle/Assets/Scripts/Puzzle.cs
+++ UntitledWordPuzzle/Assets/Scripts/Puzzle.cs
@@ -5,14 +5,21 @@ using UnityEngine.InputSystem;
 public class Puzzle : MonoBehaviour
 {
     public string CurrentLetters;
-    
     public string Solution;
     public int LetterAmount => math.max(Solution.Length, CurrentLetters.Length);
+    public Sprite solutionSprite;
 
     public Puzzle(string startWord, string solution)
     {
         CurrentLetters = startWord;
         Solution = solution;
+    }
+
+    public void SetPuzzleSolved()
+    {
+        transform.GetComponent<SpriteRenderer>().sprite = solutionSprite;
+        transform.GetComponent<BoxCollider2D>().enabled = false;
+        //highlight weg?
     }
 
 }
