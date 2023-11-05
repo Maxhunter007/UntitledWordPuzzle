b566c265487547e967ca7391a0105952bd7ad167
Phil <philine.bargen@stud-mail.uni-wuerzburg.de>
2023-11-05 14:10:09 +0100

    Letters appear at right position

--- UntitledWordPuzzle/Assets/Scripts/LetterManager.cs
+++ UntitledWordPuzzle/Assets/Scripts/LetterManager.cs
@@ -1,37 +1,46 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class LetterManager : MonoBehaviour
 {
     [SerializeField] private GameObject ContainerPrefab;
+    [SerializeField] private GameObject LetterPrefab;
 
     [SerializeField] private float ContainerDistance;
 
     private Puzzle _activePuzzle;
     private GameObject[] _containers;
-    private Sprite _aSprite;
+    private GameObject[] _letters;
     private Sprite _boxSprite;
 
     public void Awake()
     {
-        _aSprite = Resources.Load<Sprite>("Pics/A");
         _boxSprite = Resources.Load<Sprite>("Pics/Box");
     }
 
     public void Enable(Puzzle puzzle)
     {
         _activePuzzle = puzzle;
-        _aSprite = Resources.Load<Sprite>("Pics/A");   //muss später raus
-        _boxSprite = Resources.Load<Sprite>("Pics/Box");
         _containers = new GameObject[_activePuzzle.LetterAmount];
+        _letters = new GameObject[_activePuzzle.CurrentLetters.Length];
 
         var initializationPosition = -(ContainerDistance + _boxSprite.rect.width * ContainerPrefab.transform.localScale.x / _boxSprite.pixelsPerUnit / 2) * (_activePuzzle.LetterAmount - 1);
 
         for (int i = 0; i < _activePuzzle.LetterAmount; i++)
         {
-            var gameObject = Instantiate(ContainerPrefab, transform.parent);
-            gameObject.transform.localPosition = new Vector3(initializationPosition, 7f);
+            var container = Instantiate(ContainerPrefab, transform.parent);
+            container.transform.localPosition = new Vector3(initializationPosition, 7f);
             initializationPosition += 2 * ContainerPrefab.transform.localScale.x * (ContainerDistance + _boxSprite.rect.width / _boxSprite.pixelsPerUnit / 2);
-            _containers[i] = gameObject;
+            _containers[i] = container;
+
+            if (i < _activePuzzle.CurrentLetters.Length)
+            {
+                Vector3 position = new(container.transform.position.x, container.transform.position.y);
+                var letter = Instantiate(LetterPrefab, position, Quaternion.identity);
+                letter.GetComponent<TMP_Text>().text = "" + _activePuzzle.CurrentLetters[i];
+                _letters[i] = letter;
+            }
         }
     }
 
@@ -44,6 +53,12 @@ public class LetterManager : MonoBehaviour
                 Destroy(_containers[i]);
             }
             _containers = null;
+
+            for (int i = 0; i < _letters.Length; i++)
+            {
+                Destroy(_letters[i]);
+            }
+            _letters = null;
         }
     }
 
